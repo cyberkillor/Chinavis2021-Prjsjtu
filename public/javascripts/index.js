fcabc66131b61fdce8c5b668e0d0857e9fdf1398
cyberkillor <842693999@qq.com>
2021-05-26 17:19:53 +0800

    update mainpage

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -9,11 +9,17 @@ let dataSet = {
     data: []
 };
 
+let mmap = new AMap.Map("container", {
+    resizeEnable: true,
+    center: [121.4737, 31.2304],
+    zoom: 5
+});
+
 bindDirt();
 let pollutant, year, month, day, mode;
 
 function bindDirt(){
-    let btns = document.getElementsByClassName("btn");
+    let btns = document.getElementsByClassName("btn-dirt");
     // console.log(btns);
     // console.log(btns.length);
     for(let i = 0; i < btns.length; i++){
@@ -64,7 +70,7 @@ function fetchData() {
     }
 
     // return null;
-    month = year+month;
+    let ym = year+month;
 
     let headers = new Headers();
     let username = "share";
@@ -72,7 +78,7 @@ function fetchData() {
 
     headers.append('Authorization', 'Basic ' + btoa(username + ':' + password));
 
-    let url = `https://nas.tonychen.page:5006/WebDavShare/ChinaVis%202021%20Data/${month}/CN-Reanalysis-daily-${month}${day}00.csv`;
+    let url = `https://nas.tonychen.page:5006/WebDavShare/ChinaVis%202021%20Data/${ym}/CN-Reanalysis-daily-${ym}${day}00.csv`;
 
     console.log(url);
     fetch(url, {headers: headers})
@@ -96,7 +102,7 @@ function fetchData() {
                 }
             });
             // return heatmapData;
-            createMap(data);
+            createMap(dataSet.data);
         })
 }
 
@@ -105,7 +111,7 @@ function createMap(data) {
     let map = new AMap.Map("container", {
         resizeEnable: true,
         center: [121.4737, 31.2304],
-        zoom: 10
+        zoom: 5
     });
     
     if (!isSupportCanvas()) {

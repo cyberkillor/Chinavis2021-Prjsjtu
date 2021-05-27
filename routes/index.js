f124ec4b30b835d7ce8a164c12e67d232814a1bc
Tony Chen <git@tonychen.page>
2021-05-27 14:37:00 +0800

    database design

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const getDB = require('./database');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,6 +9,10 @@ router.get('/', function(req, res, next) {
 
 router.get('/index_chart', function(req, res, next) {
   res.render('index_chart', { title: 'Express' });
+});
+
+router.get('/db/:stmt', function(req, res, next) {
+  res.json(getDB(req.params.stmt));
 });
 
 
